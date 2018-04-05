01d185a944fd30a55575ce8ce4af0636ec62f902
Carlos Brito Lage <carlos@carloslage.net>
2018-04-05 18:43:14 +0100

    woop

---
+++ mix.exs
@@ -0,0 +1,25 @@
+defmodule JSONDoc.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :jsondoc,
+      version: "0.7.0",
+      language: :erlang,
+      erlc_options: [{:platform_define, "R1(1|2|3|4|5|6)", 'JSONDOC_NO_MAPS'}],
+      deps: deps(Mix.env()),
+      description: "Erlang service handling json documents and templates",
+      package: package(),
+      source_url: "https://github.com/aruki-delivery/jsondoc",
+      homepage_url: "https://hex.pm/packages/jsondoc"]
+  end
+
+  defp deps(_) do
+    [{:ex_doc, ">= 0.0.0", only: :dev}]
+  end
+
+  def package do
+    [ maintainers: ["cblage"],
+      licenses: ["Apache License 2.0"],
+      links: %{"GitHub" => "https://github.com/aruki-delivery/jsondoc" } ]
+  end
+end
---
+++ package.exs
@@ -0,0 +1,10 @@
+Expm.Package.new(
+  name: "jsondoc",
+  description: "Web Application Server",
+  version: "0.7.0",
+  keywords: ["erlang", "json", "templates", "docs"],
+  licenses: [[name: "Apache License 2.0"]],
+  maintainers: [
+    [name: "Joaquim Rocha",  email: "jrocha.github@gmailbox.org"],
+    [name: "Carlos Brito Lage",  email: "carlos@carloslage.net"]],
+  repositories: [[github: "aruki-delivery/jsondoc", branch: "master"]])
